5881f6969226d86b3a2904b778c155acc031a18e
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-06-07 13:46:40 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+response_API = requests.get("https://random-word-api.herokuapp.com/all")
+print(response_API.status_code)
