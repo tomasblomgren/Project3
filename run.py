6149a1b610eb29dfa0ecfb482a89fcb469ff8e39
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-06-07 15:07:13 +0000

    adding letter rules and messages

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
+import random
+import string
 import requests
+from requests import request
 response_API = requests.get("https://random-word-api.herokuapp.com/all")
-print(response_API.status_code)
+
+def get_valid_word(response_API):
+    word = random.choice(response_API)
+
+    return word
+
+def hangman():
+    word = get_valid_word(response_API=)
+    word_letter = set(word) # letter in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+
+    #getting user input
+    user_letter = input('Gissa en bokstav: ').upper()
+    if used_letter in alphabet - used_letters:
+        used_letters.add(used_letter)
+        if used_letters in word_letters:
+            word_letter.remove(used_letter)
+    elif user_letter in used_letters:
+        print('you have already used this letter! Please try again')
+    else:
+        ('invalid character. Please try again')
+
+user_input = input('skriv något')
+print(user_input)
